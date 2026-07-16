97431d0bc7156e4f7349f5d64f19ffbd69c826fc
mcusib-lang <mcusib@gmail.com>
2026-07-16 13:28:15 +0200

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://hhlvzsjlctmmehwmpsab.supabase.co',
-  'sb_publishable_0VVPGgjXosYhKf59jiGgVQ_ImR6T9dJ'
-)
